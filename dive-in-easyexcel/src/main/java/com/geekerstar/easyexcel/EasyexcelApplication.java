5f5fe90d79d465474cf36cd77134e2fd3c0eab4d
geekerstar <247507792@qq.com>
2019-09-19 22:24:23 +0800

    EasyExcel测试

---
+++ dive-in-easyexcel/src/main/java/com/geekerstar/easyexcel/EasyexcelApplication.java
@@ -0,0 +1,16 @@
+package com.geekerstar.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author geekerstar
+ * date: 2019/9/19 21:37
+ * description:
+ */
+@SpringBootApplication
+public class EasyexcelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyexcelApplication.class,args);
+    }
+}
